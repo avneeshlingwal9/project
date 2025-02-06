1ee24a24f8d5c2cb343d7201ac97dd74aac41a4a
avneeshlingwal9 <avneeshlingwal@gmail.com>
2025-02-06 17:59:45 +0530

    Added animations page

--- pages/pages/base/urls.py
+++ pages/pages/base/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('' , views.video_page , name = "video_page")
+    path('' , views.video_page , name = "video_page"),
+    path('page1' , views.loading_animation , name = "animation_page")
 ]
--- pages/pages/base/views.py
+++ pages/pages/base/views.py
@@ -3,3 +3,5 @@ from django.http import HttpResponse
 # Create your views here.
 def video_page(request):
     return render(request, 'base/home.html')
+def loading_animation(request):
+    return render(request, 'base/loading_animation.html')
