8d6fde784ee37c7eeaa7822e38b328f5648f6383
avneeshlingwal9 <avneeshlingwal@gmail.com>
2025-02-15 18:05:17 +0530

    Added buttons

--- pages/pages/base/urls.py
+++ pages/pages/base/urls.py
@@ -5,5 +5,11 @@ urlpatterns = [
     path('' , views.video_page , name = "video_page"),
     path('page1' , views.loading_animation , name = "animation_page"),
     path('page2' , views.scroll_bar , name = "scroll_bar"),
-    path('page3' , views.image_transition , name = "image_transition")
+    path('page3' , views.image_transition , name = "image_transition"),
+    path('btn1', views.btn1 , name = "btn1" ),
+    path('btn2', views.btn2 , name = "btn2" ),
+    path('btn3', views.btn3 , name = "btn3" ),
+    path('btn4', views.btn4 , name = "btn4" ),
+    path('btn5', views.btn5 , name = "btn5" ),
+
 ]
--- pages/pages/base/views.py
+++ pages/pages/base/views.py
@@ -9,3 +9,13 @@ def scroll_bar(request):
     return render(request , 'base/scroll_bar.html')
 def image_transition(request):
     return render(request , 'base/image_transition.html')
+def btn1(request):
+    return render(request, 'base/btn1.html')
+def btn2(request):
+    return render(request, 'base/btn2.html')
+def btn3(request):
+    return render(request, 'base/btn3.html')
+def btn4(request):
+    return render(request, 'base/btn4.html')
+def btn5(request):
+    return render(request, 'base/btn5.html')
